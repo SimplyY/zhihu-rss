7cd542f1b7b61ec9266f0131236dc665eb1d3348
SimplyY <SimplyYu@163.com>
2015-07-16 17:13:23 +0800

    测试使用了webview
    使用字符串拼接完成了css加载的问题

---
+++ test/button_webview/test.py
@@ -0,0 +1,39 @@
+import zhihu
+import MyApp
+
+from PyQt5.QtCore import QObject
+
+
+def load_css(html):
+    css = '<link href="https://raw.githubusercontent.com/SimplyY/save/master/github-markdown.css" ' \
+          'rel="stylesheet"></link>'
+
+    index = html.find('</head>')
+    new_html = html[:index] + css + html[index:]
+    return new_html
+
+
+def get_answer(root_item):
+    url = 'http://www.zhihu.com/question/31862619/answer/53611303'
+    answer = zhihu.Answer(url)
+
+    html = answer.content
+    new_html = load_css(html)
+
+    web_view = root_item.findChild(QObject, 'web_view')
+    web_view.loadHtml(new_html)
+
+
+def set_view(viewer):
+    root_item = viewer.rootObject()
+
+    button = root_item.findChild(QObject, 'button')
+    button.clicked.connect(lambda: get_answer(root_item))
+
+
+if __name__ == '__main__':
+    my_app = MyApp(qml='test.qml')
+
+    set_view(my_app.viewer)
+
+    MyApp.show(my_app)
