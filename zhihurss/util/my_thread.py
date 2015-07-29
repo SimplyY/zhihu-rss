666d609fe6dcef166db4c8eba27c641b3d6d05fb
Wang Xuerui <idontknw.wang@gmail.com>
2015-07-29 18:36:05 +0800

    fix ctor call

--- zhihurss/util/my_thread.py
+++ zhihurss/util/my_thread.py
@@ -5,7 +5,7 @@ import threading
 
 class MyThread(threading.Thread):
     def __init__(self, threadname, fun, *args):
-        super().__init__(self, name=threadname)
+        super().__init__(name=threadname)
         self.name = threadname
         self.fun = fun
         self.args = args
